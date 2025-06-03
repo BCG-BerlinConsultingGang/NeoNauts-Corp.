14b1137c428e9ea26991ba259ad958f9010c70ea
Alexander Winkler <alexander.winkler8@gmail.com>
2025-06-03 14:45:35 +0000

    updated transactions table with average transactions per day

--- models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_transactions.sql
+++ models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_transactions.sql
@@ -18,31 +18,42 @@ WITH cleaned_transactions AS (
         CAST(REPLACE(user_id, "user_", "") AS integer) AS user_id,
         DATE(created_date) AS transaction_date
     FROM {{ ref('stg_raw_neo_bank__transactions') }}
+),
+
+aggregated AS (
+    SELECT
+        user_id,
+        COUNT(*) AS total_transactions,
+        ROUND(SUM(amount_usd), 2) AS total_amount_usd,
+        ROUND(AVG(amount_usd), 2) AS average_amount_per_transaction_usd,
+        MIN(transaction_date) AS first_transaction_date,
+        MAX(transaction_date) AS last_transaction_date,
+        DATE_DIFF(MAX(transaction_date), MIN(transaction_date), DAY) AS time_between_transactions,
+
+        -- Transactions Type breakdown
+        COUNTIF(transactions_type = "REFUND") AS transactions_type_refund,
+        COUNTIF(transactions_type = "TAX") AS transactions_type_tax,
+        COUNTIF(transactions_type = "CARD_REFUND") AS transactions_type_card_refund,
+        COUNTIF(transactions_type = "FEE") AS transactions_type_fee,
+        COUNTIF(transactions_type = "CASHBACK") AS transactions_type_cashback,
+        COUNTIF(transactions_type = "ATM") AS transactions_type_atm,
+        COUNTIF(transactions_type = "EXCHANGE") AS transactions_type_exchange,
+        COUNTIF(transactions_type = "TOPUP") AS transactions_type_topup,
+        COUNTIF(transactions_type = "TRANSFER") AS transactions_type_transfer,
+        COUNTIF(transactions_type = "CARD_PAYMENT") AS transactions_type_card_payment,
+
+        -- Direction breakdown
+        COUNTIF(direction = "INBOUND") AS direction_inbound,
+        COUNTIF(direction = "OUTBOUND") AS direction_outbound
+
+    FROM cleaned_transactions
+    GROUP BY user_id
 )
 
 SELECT
-    CAST(user_id as integer) as user_id,
-    COUNT(*) AS total_transactions,
-    ROUND(SUM(amount_usd),2) AS total_amount_usd,
-    ROUND(AVG(amount_usd),2) AS average_amount_per_transaction_usd,
-    MIN(transaction_date) AS first_transaction_date,
-    MAX(transaction_date) AS last_transaction_date,
-
-    -- Transactions Type breakdown (e.g. "card_payment", "refund" etc.)
-    COUNTIF(transactions_type = "REFUND") AS transactions_type_refund,
-    COUNTIF(transactions_type = "TAX") AS transactions_type_tax,
-    COUNTIF(transactions_type = "CARD_REFUND") AS transactions_type_card_refund,
-    COUNTIF(transactions_type = "FEE") AS transactions_type_fee,
-    COUNTIF(transactions_type = "CASHBACK") AS transactions_type_cashback,
-    COUNTIF(transactions_type = "ATM") AS transactions_type_atm,
-    COUNTIF(transactions_type = "EXCHANGE") AS transactions_type_exchange,
-    COUNTIF(transactions_type = "TOPUP") AS transactions_type_topup,
-    COUNTIF(transactions_type = "TRANSFER") AS transactions_type_transfer,
-    COUNTIF(transactions_type = "CARD_PAYMENT") AS transactions_type_card_payment,
-
-    -- Direction breakdown (e.g., "inbound", "outbound")
-    COUNTIF(direction = "INBOUND") AS direction_inbound,
-    COUNTIF(direction = "OUTBOUND") AS direction_outbound
-
-FROM cleaned_transactions
-GROUP BY user_id
+    *,
+    ROUND(SAFE_DIVIDE(
+        total_transactions,
+        time_between_transactions + 1
+    ),2) AS avg_transactions_per_day
+FROM aggregated
