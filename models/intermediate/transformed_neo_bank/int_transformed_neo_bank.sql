9bae340519ccf47e109d6d9eb725956ddade18e2
August Zoellner <august.zoellner@gmail.com>
2025-06-05 10:32:58 +0000

    included active column

--- models/intermediate/transformed_neo_bank/int_transformed_neo_bank.sql
+++ models/intermediate/transformed_neo_bank/int_transformed_neo_bank.sql
@@ -17,8 +17,10 @@ with subquery as(
 select 
     s.*, 
     t.* except(user_id),
-    DATE_DIFF(t.last_transaction_date, s.sign_up_date, DAY) as active_timeframe
+    DATE_DIFF(t.last_transaction_date, s.sign_up_date, DAY) as active_timeframe,
+    IF(t.days_since_last_transaction <= 30, 1, 0) as active_user -- users without transaction in last 30 days are considered as inactive users (hard churn threshold)
     from subquery as s 
         INNER join {{ ref('int_cleaned_neo_bank_transactions') }} as t 
         USING(user_id)
+        order by t.days_since_last_transaction asc
         
