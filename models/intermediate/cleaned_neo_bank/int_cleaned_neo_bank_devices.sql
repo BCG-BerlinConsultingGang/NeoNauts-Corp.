774950edc8743619536a63ecbd9c3b548fc930dd
August Zoellner <august.zoellner@gmail.com>
2025-06-06 09:22:45 +0000

    Created cleaned devices table bc naming was done incorrect. Additionally I created a second subquery in the intermediate table to include the device brand per user

--- models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_devices.sql
+++ models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_devices.sql
@@ -1,2 +1,5 @@
-SELECT *
-FROM {{ ref('stg_raw_neo_bank__decices') }}
+-- want to get rid of unclear device brands: "unknown" & "brand"
+
+SELECT  user_id, device_brand
+FROM {{ ref('stg_raw_neo_bank__devices') }}
+WHERE device_brand IN ('Apple', 'Android')
