d895f4f23742114c1a11c3b2df3b57d0ff57174b
August Zoellner <august.zoellner@gmail.com>
2025-06-06 09:24:37 +0000

    changed devices user_id to integer

--- models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_devices.sql
+++ models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_devices.sql
@@ -1,5 +1,5 @@
 -- want to get rid of unclear device brands: "unknown" & "brand"
 
-SELECT  user_id, device_brand
+SELECT  cast(regexp_replace(user_id, '^user_', '') as integer) as user_id, device_brand
 FROM {{ ref('stg_raw_neo_bank__devices') }}
 WHERE device_brand IN ('Apple', 'Android')
