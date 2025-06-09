7e9a92c45c451a3a0840b13b4f45b26210be2913
August Zoellner <august.zoellner@gmail.com>
2025-06-09 09:50:13 +0000

    changed devices table

--- models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_devices.sql
+++ models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_devices.sql
@@ -2,7 +2,7 @@
 
 SELECT  cast(regexp_replace(user_id, '^user_', '') as integer) as user_id, 
 device_brand, 
-CAST(device_brand = "Apple" AS INT64) AS apple_user,
+CAST(device_brand = "Apple" AS INT64)  AS apple_user,
 CAST(device_brand = "Android" AS INT64) AS android_user,
 FROM {{ ref('stg_raw_neo_bank__devices') }}
 WHERE device_brand IN ('Apple', 'Android')
