c29d97436e48d3cbf0ea3268493dfd2f5412f9d7
Alex Winkler <alexander.winkler8@gmail.com>
2025-06-20 15:40:01 +0200

    Delete models directory

--- models/intermediate/cleaned_neo_bank/int_cleaned_neo_bank_devices.sql
+++
@@ -1,5 +0,0 @@
--- Getting rid of unclear device brands: "unknown" & "brand"
-
-SELECT  cast(regexp_replace(user_id, '^user_', '') as integer) as user_id, device_brand
-FROM {{ ref('stg_raw_neo_bank__devices') }}
-WHERE device_brand IN ('Apple', 'Android')
